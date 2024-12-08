e188475b7c6d87cfdfbf9ce8319e82c7e7c2df0b
Andrei Cojocariu <73075294+ACOJO5920@users.noreply.github.com>
2024-12-08 16:38:58 +0000

    Update day8part2.py

--- day8part2.py
+++ day8part2.py
@@ -35,17 +35,6 @@ def run(filename: str) -> int:
 						antinodes.append(new_antinode)
 	return len(antinodes)
 
-def find_antinodes_of_two_antennas(antenna_1: Antenna, antenna_2: Antenna) -> tuple[Antinode, Antinode] | None:
-	if antenna_1.value != antenna_2.value:
-		return None
-	if antenna_1 == antenna_2:
-		return None
-	x = antenna_2.x - antenna_1.x
-	y = antenna_2.y - antenna_1.y
-	antinode_1 = Antinode(value=antenna_1.value, x=antenna_1.x-x, y=antenna_1.y-y)
-	antinode_2 = Antinode(value=antenna_1.value, x=antenna_2.x+x, y=antenna_2.y+y)	
-	return antinode_1, antinode_2	
-
 def find_antinodes_v2(antenna_1: Antenna, antenna_2: Antenna, x_limit: int, y_limit: int) -> list[Antinode] | None:
 	if antenna_1.value != antenna_2.value:
 		return None
